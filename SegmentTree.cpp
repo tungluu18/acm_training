eb4ade5b20c5a0554c3e4f52d2350dc081bd0b3d
tungluu18 <tungluu18@gmail.com>
2017-10-25 21:26:14 +0700

    update seggment tree lazy update
    update seggment tree lazy update

--- SegmentTree.cpp
+++ SegmentTree.cpp
@@ -1,66 +1,47 @@
-#include <bits/stdc++.h>
+void build(int node = 1, int left = 1, int right = n) {
+    if (left == right) {
+        tree[node] = {c[left], 0};
+        return;
+    }
+    int mid = (left + right) / 2;
+    build(node * 2, left, mid);
+    build(node * 2 + 1, mid + 1, right);
+    tree[node] = {tree[node * 2].fi + tree[node * 2 + 1].fi, 0};
+}
 
-using namespace std;
+void lazy(int node, int ll, int rr) {
+    if (tree[node].se & 1) {
+        tree[node].fi = (rr - ll + 1) - tree[node].fi;  ///switch
+    }
+    if (ll < rr) {
+        tree[node * 2].se += tree[node].se;
+        tree[node * 2 + 1].se += tree[node].se;
+    }
+    tree[node].se = 0;
+}
 
-class SegmentTree {
-private :
-    vector <int> T, lazy;
-    int n;
+int get(int u, int v, int node = 1, int ll = 1, int rr = n) {
+    lazy(node, ll, rr);
 
-    void passInfor(int node, int nodeL, int nodeR) {
-        if (lazy[node] == -1) return;
-        T[node] = lazy[node];
-        if (nodeL == nodeR) return;
-        lazy[2 * node] = lazy[2 * node + 1] = lazy[node];
-        lazy[node] = -1;
+    if (u > rr || v < ll) return 0;
+    if (u <= ll && rr <= v) return tree[node].fi;
+
+    int mi = (ll + rr) / 2;
+    return get(u, v, node * 2, ll, mi) + get(u, v, node * 2 + 1, mi + 1, rr);
+}
+
+void update(int u, int v, int node = 1, int ll = 1, int rr = n) {
+    lazy(node, ll, rr);
+
+    if (u > rr || v < ll) return;
+    if (u <= ll && rr <= v) {
+        tree[node].se = 1;
+        lazy(node, ll, rr);
+        return;
     }
 
-    void update(int node, int nodeL, int nodeR, int left, int right, int value) {
-        passInfor(node, nodeL, nodeR);
-        if (nodeR < left || nodeL > right) return;
-        if (left <= nodeL && nodeR <= right) {
-            lazy[node] = value;
-            passInfor(node, nodeL, nodeR);
-            return;
-        }
-        int nodeMid = (nodeL + nodeR) / 2;
-        update(node * 2, nodeL, nodeMid, left, right, value);
-        update(node * 2 + 1, nodeMid + 1, nodeR, left, right, value);
-        T[node] = max(T[2 * node], T[2 * node + 1]);
-    }
-
-    int get(int node, int nodeL, int nodeR, int left, int right) {
-        passInfor(node, nodeL, nodeR);
-        if (nodeR < left || nodeL > right) return -1e9;
-        if (left <= nodeL && nodeR <= right) {
-            return T[node];
-        }
-        int nodeMid = (nodeL + nodeR) / 2;
-        return max(get(2 * node, nodeL, nodeMid, left, right),
-                   get(2 * node + 1, nodeMid + 1, nodeR, left, right));
-    }
-
-public :
-    SegmentTree(int n) {
-        this->n = n;
-        T.resize(4 * n);
-        lazy.resize(4 * n);
-    }
-
-    void update(int left, int right, int value) {
-        update(1, 1, n, left, right, value);
-    }
-
-    int get(int left, int right) {
-        return get(1, 1, n, left, right);
-    }
-};
-
-
-int main() {
-    SegmentTree IT(4);
-    IT.update(2, 3, 4);
-    IT.update(1, 2, 5);
-    IT.update(3, 3, 10);
-    cout << IT.get(2, 4);
+    int mi = (ll + rr) / 2;
+    update(u, v, node * 2, ll, mi);
+    update(u, v, node * 2 + 1, mi + 1, rr);
+    tree[node].fi = tree[node * 2].fi + tree[node * 2 + 1].fi;
 }
